aad71f53e52731ae8cb41d0293d3d250be8ec406
prolajumokeoni <prolajumokeoni@gmail.com>
2023-10-21 14:42:05 +0100

    Setup routes  for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route} from 'react-router-dom';
+import Coins from './components/Coins';
+import ExchangeRate from './components/ExchangeRate';
+import { SignIn } from './components/SignIn';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+    <Route path="/" element={<Coins/>} />
+    <Route path="/exchangerate" element={<ExchangeRate/>} />
+    <Route path="/exchangerate" element={<ExchangeRate/>} />
+    <Route path="/SignIn" element={<SignIn/>} />
+    <Route path="/Sign" element={<SignIn/>} />
+</Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ExchangeRate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExchangeRate = () => {
+	return (
+		<div>ExchangeRate</div>
+	)
+}
+
+export default ExchangeRate
